b1b723370375e1b1141cd7955916d1cf555f01c7
lgarczyn <lgarczyn@student.42.fr>
2019-11-07 13:24:31 +0100

    optimized worst case sort scenario

--- sort.c
+++ sort.c
@@ -6,13 +6,13 @@
 /*   By: lgarczyn <lgarczyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/02 14:53:46 by lgarczyn          #+#    #+#             */
-/*   Updated: 2019/11/07 13:00:21 by lgarczyn         ###   ########.fr       */
+/*   Updated: 2019/11/07 13:23:48 by lgarczyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void				insert(t_file **list, t_file *file, t_cmp *f)
+static void			insert(t_file **list, t_file *file, t_cmp *f)
 {
 	while (*list && f(*list, file))
 	{
@@ -26,14 +26,25 @@ void				sort_list(t_file **head, t_cmp *f)
 {
 	t_file			*list;
 	t_file			*prev;
+	t_file			*last;
 
-	list = *head;
-	*head = NULL;
+	prev = *head;
+	list = prev->next;
+	prev->next = NULL;
+	*head = prev;
+	last = prev;
 	while (list)
 	{
 		prev = list;
 		list = list->next;
-		insert(head, prev, f);
+		if (f(last, prev))
+		{
+			prev->next = NULL;
+			last->next = prev;
+			last = prev;
+		}
+		else
+			insert(head, prev, f);
 	}
 }
 
